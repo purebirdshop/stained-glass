f2ded88dd2867cf2263f88dc637b35d3e02329c1
purebirdshop <jason@thebirdshop.co>
2026-03-25 11:41:24 -0700

    cleaned up labeling

--- src/data/categoryGroups.js
+++ src/data/categoryGroups.js
@@ -1,11 +1,11 @@
 const categoryGroups = {
  "adults":[302229],
  "kids":[302227,306757,302225],
- "junior-high":[304721,304720],
- "high-school": [652218,648461],
- "high-cards":[391753,644934],
- "unique-dna":[633276,302233],
- "assigned-volunteers":[679252]
+ "junior High":[304721,304720],
+ "high School": [652218,648461],
+ "high Cards":[391753,644934],
+ "DNA`":[633276,302233],
+ "Assigned Volunteers":[679252]
 }
 
 export default categoryGroups;
